1c575241592a6598e5a364913a465ede4b5593e4
Vítor Araujo <vitoraraujo@tuta.com>
2026-06-16 14:09:02 -0300

    build: improve install script
    Add print statements and check if hook is
    properly configured

--- install.sh
+++ install.sh
@@ -1,5 +1,18 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
+echo "Copying file"
 install -D ./commit_msg.py ~/.githooks/commit-msg
+
+echo "Making it executable"
 chmod +x ~/.githooks/commit-msg
+
+echo "Adding global git hooks path"
 git config --global core.hooksPath  ~/.githooks
+
+if git hook list commit-msg >/dev/null 2>&1; then
+    echo "Done"
+else
+    echo "Failed to create hook"
+    echo Aborting
+    exit 1
+fi
